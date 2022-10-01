4173b353e129c1874bd24544efae1a38d391596f
Morph-WOLF <99325386+Morph-WOLF@users.noreply.github.com>
2022-10-01 18:55:29 +0530

    Add files via upload

--- application_auto_filler_v2.py
+++ application_auto_filler_v2.py
@@ -1,19 +1,22 @@
+#Importing only neccesary things to save processing time
 from pyautogui import moveTo, FAILSAFE, click, scroll, hotkey, press, pixelMatchesColor
 from pynput.mouse import Events as event
 from pynput.mouse import Controller as Mcontroller
-mouse = Mcontroller()
-current_cycle = 0
-delay = event()
+
+mouse = Mcontroller() #Assigning Controllers to a var for easiar use.
+current_cycle = 0 #the cycle its currently on.
+delay = event() #to add a delay in-between cetain actions.
 delay.get(3)
 FAILSAFE = True
-while current_cycle != 100:
+
+while current_cycle != 1: #Start of the loop change the num after "!=" to the amount of applications you need to fill.
     at_right_place = 0
-    moveTo(1010, 344, .2)
+    moveTo(1025, 372, .2)
     click()
     delay.get(.9)
     mouse.scroll(0,-20)
 
-    while at_right_place != 1:
+    while at_right_place != 1:#confirming if the script has scrolled to the bottom, by checking if the color of a pixiel in bottom left, if its grey
         if pixelMatchesColor(1365, 763,(135, 135, 135)) == True :
             delay.get(0.3)
             moveTo(688, 650,)
@@ -27,14 +30,15 @@ while current_cycle != 100:
     press('esc')
 
     delay.get(.5)
-    moveTo(1128, 333)
+    moveTo(1128, 372)
     click()
 
     delay.get(.7)
-    moveTo(688, 212)
+    moveTo(688, 236)
     click()
+
     hotkey('ctrl', 'v')
-    moveTo(883, 446)
+    moveTo(883, 465)
     click()
 
     delay.get(3.5)
